c49a3dc70159c364aa8a4023e33d46d7641e6a43
Tyron Lardy <t.d.lardy@math.leidenuniv.nl>
2023-03-31 10:43:40 +0200

    logistic_i_job: include plotting

--- simulation_1/sim_logistic_1_job.sh
+++ simulation_1/sim_logistic_1_job.sh
@@ -8,6 +8,7 @@ jid1=$(sbatch --job-name="sim_logistic_1" --parsable simulation_1/sim_logistic_1
 
 # Second job collecting the results of first
 
-sbatch --job-name="sim_logistic_1_collect" --dependency=afterany:$jid1 simulation_1/sim_logistic_1_collect.sh
+jid2=$(sbatch --job-name="sim_logistic_1_collect" --dependency=afterany:$jid1 --export= simulation_1/sim_logistic_1_collect.sh)
 
+sbatch --job-name="sim_logistic_1_plot" --dependency=afterany:$jid2 --parsable --export=eps=0.05,path="simulation_1.rda" simulation_1/sim_logistic_1_plot.sh
 
--- simulation_2/sim_logistic_2_job.sh
+++ simulation_2/sim_logistic_2_job.sh
@@ -8,6 +8,7 @@ jid1=$(sbatch --job-name="sim_logistic_2" --parsable simulation_2/sim_logistic_2
 
 # Second job collecting the results of first
 
-sbatch --job-name="sim_logistic_2_collect" --dependency=afterany:$jid1 simulation_2/sim_logistic_2_collect.sh
+jid2=$(sbatch --job-name="sim_logistic_2_collect" --dependency=afterany:$jid1 simulation_2/sim_logistic_2_collect.sh)
 
+sbatch --job-name="sim_logistic_2_plot" --dependency=afterany:$jid2 --parsable --export=eps=path="simulation_2.rda" simulation_2/sim_logistic_2_plot.sh
 
